5a557acc6e7e3ea880a2c383e79b9995ea4fb09d
Shivit Kapoor <shivit2004@gmail.com>
2025-01-13 13:37:25 +0000

    [sk3122] - Update fontSize and fontWeight of exercise_appBar

--- llm_based_sat_app/lib/widgets/exercise_widgets/exercise_appBar.dart
+++ llm_based_sat_app/lib/widgets/exercise_widgets/exercise_appBar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class ExerciseAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
-  final Color backgroundColor;
   final double fontSize;
   final FontWeight fontWeight;
   final Color textColor;
@@ -11,17 +10,15 @@ class ExerciseAppBar extends StatelessWidget implements PreferredSizeWidget {
   const ExerciseAppBar({
     Key? key,
     required this.title,
-    this.backgroundColor = Colors.blueAccent,
     this.fontSize = 20.0,
-    this.fontWeight = FontWeight.bold,
-    this.textColor = Colors.white,
+    this.fontWeight = FontWeight.w600,
+    this.textColor = const Color(0xFF687078),
     this.padding = const EdgeInsets.symmetric(horizontal: 16.0),
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      backgroundColor: backgroundColor,
       title: Padding(
         padding: padding,
         child: Text(
