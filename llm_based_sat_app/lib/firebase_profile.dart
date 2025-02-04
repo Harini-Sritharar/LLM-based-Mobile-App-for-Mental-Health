b69ffa4830ff7157f6ad6da681e975303bf95129
15skumar <shivangi.kumar@hotmail.co.uk>
2025-02-04 20:12:41 +0000

    [sk2922, hs1922] Feat: Add overall score calculation and linked to backend

--- llm_based_sat_app/lib/firebase_profile.dart
+++ llm_based_sat_app/lib/firebase_profile.dart
@@ -63,7 +63,9 @@ Future<void> saveQuestionnaireResponse(
   CollectionReference responsesRef = userDoc.collection('responses');
 
   // Store the questionnaire response
-  await responsesRef.doc().set({
+  DocumentReference docRef = responsesRef.doc();
+  String docId = docRef.id;
+  await docRef.set({
     'name': name,
     'timestamp': FieldValue.serverTimestamp(),
     'answers':
@@ -73,25 +75,90 @@ Future<void> saveQuestionnaireResponse(
   // Check if all required questionnaires are completed
   DocumentSnapshot userSnapshot = await userDoc.get();
   // Ensure completedQuestionnaires exists
-  List<String> completedQuestionnaires = [];
+  Map<String, String> completedQuestionnaires = {};
   if (userSnapshot.exists && userSnapshot.data() != null) {
     var data = userSnapshot.data() as Map<String, dynamic>;
     if (data.containsKey('completedQuestionnaires')) {
       completedQuestionnaires =
-          List<String>.from(data['completedQuestionnaires']);
+          Map<String, String>.from(data['completedQuestionnaires']);
     }
   }
 
-  if (!completedQuestionnaires.contains(name)) {
-    completedQuestionnaires.add(name);
-  }
+  completedQuestionnaires[name] = docId;
 
   await userDoc.update({'completedQuestionnaires': completedQuestionnaires});
 
-  // // Recalculate overall score if all questionnaires are completed
-  // if (completedQuestionnaires.length == 6) {
-  //   await recalculateScores(user.uid);
-  // }
+  // Recalculate overall score if all questionnaires are completed
+  if (completedQuestionnaires.length == 6) {
+    print(" length 6 ");
+    await recalculateScores(user.uid);
+  }
+}
+
+/// Recalculates the overall score and subscale scores
+Future<void> recalculateScores(String userId) async {
+  Map<String, int> subScores = {
+    'GAD-7': 0,
+    'PHQ-9': 0,
+    'PWB': 0,
+    'SOCS-S': 0,
+    'CPC-12R': 0,
+    'ERQ_R': 0,
+    'ERQ_S': 0,
+  };
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  DocumentReference userDoc = db.collection('Profile').doc(userId);
+
+  DocumentSnapshot userSnapshot = await userDoc.get();
+  var data = userSnapshot.data() as Map<String, dynamic>;
+  Map<String, String> completedQuestionnaires =
+      Map<String, String>.from(data['completedQuestionnaires']);
+
+  // Iterate through each completed questionnaire in the map
+  completedQuestionnaires.forEach((name, docId) async {
+    DocumentSnapshot responseSnapshot =
+        await userDoc.collection('responses').doc(docId).get();
+    var responseData = responseSnapshot.data() as Map<String, dynamic>;
+    Map<String, String> answers =
+        Map<String, String>.from(responseData['answers']);
+
+    if (name == 'ERQ') {
+      int ERQ_R = 0;
+      int ERQ_S = 0;
+      answers.forEach((i, score) {
+        if ([1, 3, 5, 7, 8, 10].contains(i)) {
+          ERQ_R += int.parse(score);
+        } else {
+          ERQ_S += int.parse(score);
+        }
+      });
+      subScores.update('ERQ_R', (value) => ERQ_R);
+      subScores.update('ERQ_S', (value) => ERQ_S);
+    } else {
+      int tempScore = 0;
+      // Iterate through each set of response to add up the score for individual questionnaires
+      answers.forEach((_, score) {
+        tempScore += int.parse(score);
+      });
+      subScores.update(name, (value) => tempScore);
+    }
+  });
+
+  double mentalHealthScore =
+      1 - (0.5 * ((subScores['GAD-7']! / 21) + (subScores['PHQ-9']! / 27)));
+  double weightedScore = (0.4 * subScores['PWB']! +
+      0.25 * subScores['SOCS-S']! +
+      0.25 * subScores['CPC-12R']! +
+      0.1 * (subScores['ERQ_R']! + (28 - subScores['ERQ_S']!)));
+
+  double overallScore = (mentalHealthScore + weightedScore) / 2;
+
+  // Save the new scores
+  await userDoc.update({
+    'overallScore': overallScore,
+  });
+
+  print("Scores updated successfully!");
 }
 
 Future<bool> isPersonalInfoComplete() async {
