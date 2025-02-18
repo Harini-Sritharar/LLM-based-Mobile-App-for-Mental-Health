b75b5b20e045824817bc5df31a7fc9dc12c706a6
Shivit Kapoor <shivit2004@gmail.com>
2025-02-18 16:19:38 +0000

    [sk3122] - Update video link to contain course

--- llm_based_sat_app/lib/widgets/firebase_video_player.dart
+++ llm_based_sat_app/lib/widgets/firebase_video_player.dart
@@ -27,7 +27,7 @@ class _LocalVideoPlayerState extends State<LocalVideoPlayer> {
   Future<void> _initializeVideo() async {
     // Initialize the video controller with the provided video URL
     _controller = VideoPlayerController.networkUrl(Uri.parse(
-        'https://firebasestorage.googleapis.com/v0/b/llm-based-sat-app.firebasestorage.app/o/introductory_videos%2FKohli%20creams%20a%20classic%20cover%20drive.mp4?alt=media&token=d5c2d47f-869a-474b-90f6-40750b0b7c37'))
+        'https://firebasestorage.googleapis.com/v0/b/llm-based-sat-app.firebasestorage.app/o/introductory_videos%2F${widget.course.id}.mp4?alt=media&token=ab6614f3-d0d9-4d4e-a365-e96565039b5e'))
       ..initialize().then((_) {
         setState(() {
           _isLoading = false;
@@ -35,9 +35,13 @@ class _LocalVideoPlayerState extends State<LocalVideoPlayer> {
         _controller.setLooping(false); // Optional: Loop the video
         _controller.play(); // Optional: Auto-play the video
 
-        // Update cache and database to indicate intro video has been watched
-        CacheManager.setValue("${widget.course.id}_introductory_video", true);
-        updateWatchedIntroductoryVideo(user!.uid, widget.course.id, true);
+        // Update cache and database to indicate intro video has been watched if doesn't exist in cache
+        if (CacheManager.getValue("${widget.course.id}_introductory_video") ==
+            null || CacheManager.getValue("${widget.course.id}_introductory_video") ==
+            false) {
+          CacheManager.setValue("${widget.course.id}_introductory_video", true);
+          updateWatchedIntroductoryVideo(user!.uid, widget.course.id, true);
+        }
       });
   }
 
@@ -50,7 +54,7 @@ class _LocalVideoPlayerState extends State<LocalVideoPlayer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Local Video Player')),
+      appBar: AppBar(title: Text(widget.course.id)),
       body: Center(
         child: _isLoading
             ? const CircularProgressIndicator()
@@ -58,7 +62,8 @@ class _LocalVideoPlayerState extends State<LocalVideoPlayer> {
                 aspectRatio: _controller.value.aspectRatio,
                 child: Container(
                   width: MediaQuery.of(context).size.width * 0.9, // Limit width
-                  height: MediaQuery.of(context).size.height * 0.5, // Limit height
+                  height:
+                      MediaQuery.of(context).size.height * 0.5, // Limit height
                   child: FittedBox(
                     fit: BoxFit.contain, // Ensures the video fits within bounds
                     child: SizedBox(
@@ -74,7 +79,9 @@ class _LocalVideoPlayerState extends State<LocalVideoPlayer> {
           ? FloatingActionButton(
               onPressed: () {
                 setState(() {
-                  _controller.value.isPlaying ? _controller.pause() : _controller.play();
+                  _controller.value.isPlaying
+                      ? _controller.pause()
+                      : _controller.play();
                 });
               },
               child: Icon(
