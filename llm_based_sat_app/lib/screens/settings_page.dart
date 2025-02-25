20b98a1edc8591aa50412b38adc5b876252d651d
dk2222 <dk2222@ic.ac.uk>
2025-02-25 12:44:33 +0000

    [dk2222] fix: make settings use reusable menu items and back buttom

--- llm_based_sat_app/lib/screens/settings_page.dart
+++ llm_based_sat_app/lib/screens/settings_page.dart
@@ -7,6 +7,8 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:llm_based_sat_app/screens/language_page.dart';
 import 'package:llm_based_sat_app/screens/notifications_settings_page.dart';
 import 'package:llm_based_sat_app/widgets/custom_app_bar.dart';
+import 'package:llm_based_sat_app/widgets/custom_button.dart';
+import 'package:llm_based_sat_app/widgets/menu_item.dart';
 
 import '../widgets/main_layout.dart';
 import 'help_centre_page.dart';
@@ -42,170 +44,89 @@ class SettingsPage extends StatelessWidget {
             ),
             SizedBox(height: 10),
             // Settings options displayed as list items.
-            _buildSettingsItem(
-              context,
-              'Notifications',
-              'assets/icons/profile/notification-bing.svg',
-              () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => NotificationSettingsPage(
-                      onItemTapped: onItemTapped,
-                      selectedIndex: selectedIndex,
+            MenuItem(
+                title: 'Notifications',
+                icon: 'assets/icons/profile/notification-bing.svg',
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => NotificationSettingsPage(
+                        onItemTapped: onItemTapped,
+                        selectedIndex: selectedIndex,
+                      ),
                     ),
-                  ),
-                );
-              },
-            ),
-            _buildSettingsItem(
-              context,
-              'Security',
-              'assets/icons/profile/security-safe.svg',
-              () {},
-            ),
-            _buildSettingsItem(
-              context,
-              'Accessibility',
-              'assets/icons/profile/accessibility.svg',
-              () {},
-            ),
-            _buildSettingsItem(
-              context,
-              'Language',
-              'assets/icons/profile/language-circle.svg',
-              () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => LanguagePage(
-                      onItemTapped: onItemTapped,
-                      selectedIndex: selectedIndex,
+                  );
+                }),
+            MenuItem(
+                title: 'Security',
+                icon: 'assets/icons/profile/security-safe.svg',
+                onTap: () {}),
+            MenuItem(
+                title: 'Accessibility',
+                icon: 'assets/icons/profile/accessibility.svg',
+                onTap: () {}),
+            MenuItem(
+                title: 'Language',
+                icon: 'assets/icons/profile/language-circle.svg',
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LanguagePage(
+                        onItemTapped: onItemTapped,
+                        selectedIndex: selectedIndex,
+                      ),
                     ),
-                  ),
-                );
-              },
-            ),
-            _buildSettingsItem(
-              context,
-              'Terms & Conditions',
-              'assets/icons/profile/book.svg',
-              () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => TermsAndConditionsPage(
-                      onItemTapped: onItemTapped,
-                      selectedIndex: selectedIndex,
+                  );
+                }),
+            MenuItem(
+                title: 'Terms & Conditions',
+                icon: 'assets/icons/profile/book.svg',
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => TermsAndConditionsPage(
+                        onItemTapped: onItemTapped,
+                        selectedIndex: selectedIndex,
+                      ),
                     ),
-                  ),
-                );
-              },
-            ),
-            _buildSettingsItem(
-              context,
-              'Help Centre',
-              'assets/icons/profile/message-question.svg',
-              () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => HelpCentrePage(
-                      onItemTapped: onItemTapped,
-                      selectedIndex: selectedIndex,
+                  );
+                }),
+            MenuItem(
+                title: 'Help Centre',
+                icon: 'assets/icons/profile/message-question.svg',
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HelpCentrePage(
+                        onItemTapped: onItemTapped,
+                        selectedIndex: selectedIndex,
+                      ),
                     ),
-                  ),
-                );
-              },
-            ),
-            _buildSettingsItem(
-              context,
-              'Reset Setting',
-              'assets/icons/profile/rotate-left.svg',
-              () {},
-            ),
-            _buildSettingsItem(
-              context,
-              'Delete Account',
-              'assets/icons/profile/trash.svg',
-              () {},
-            ),
+                  );
+                }),
+            MenuItem(
+                title: 'Reset Setting',
+                icon: 'assets/icons/profile/rotate-left.svg',
+                onTap: () {}),
+            MenuItem(
+                title: 'Delete Account',
+                icon: 'assets/icons/profile/trash.svg',
+                onTap: () {}),
+
             SizedBox(height: 20),
-            _buildBackButton(
-                context), // Back button to navigate to the previous page.
+            CustomButton(
+              buttonText: "Back",
+              onPress: () => Navigator.pop(
+                  context), // Closes the current screen and returns to the previous one
+              leftArrowPresent: true,
+              rightArrowPresent: false,
+              textColor: Colors.white,
+            ), // Back button to navigate to the previous page.
           ],
-        ),
-      ),
-    );
-  }
-
-  /// Builds a list item for the settings page.
-  ///
-  /// [context]: The BuildContext of the widget.
-  /// [title]: The title displayed for the setting.
-  /// [icon]: The icon to be displayed (either a string path to an SVG or an IconData).
-  /// [onTap]: The callback function when the item is tapped.
-  Widget _buildSettingsItem(
-      BuildContext context, String title, dynamic icon, VoidCallback onTap) {
-    return ListTile(
-      leading: (icon is String)
-          ? SvgPicture.asset(
-              icon,
-              width: 24,
-              height: 24,
-              colorFilter: ColorFilter.mode(primaryTextColor, BlendMode.srcIn),
-            )
-          : Icon(icon, color: primaryTextColor),
-      title: Text(
-        title,
-        style: TextStyle(fontSize: 18, color: primaryTextColor),
-      ),
-      trailing: Icon(Icons.arrow_forward_ios, size: 18, color: arrowColor),
-      onTap: onTap,
-    );
-  }
-
-  /// Builds the back button at the bottom of the page.
-  ///
-  /// [context]: The BuildContext of the widget.
-  Widget _buildBackButton(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10),
-      child: SizedBox(
-        width: double.infinity,
-        height: 60,
-        child: ElevatedButton(
-          onPressed: () =>
-              Navigator.pop(context), // Navigate to the previous page.
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Color(0xFF1C548C),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(25),
-            ),
-          ),
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: EdgeInsets.only(
-                      left: 20), // Adjust left padding if needed.
-                  child: SvgPicture.asset(
-                    'assets/icons/back_icon.svg',
-                    width: 30,
-                    height: 30,
-                  ),
-                ),
-              ),
-              Center(
-                child: Text(
-                  "Back",
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
-              ),
-            ],
-          ),
         ),
       ),
     );
