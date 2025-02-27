5438ace9c186d6bc52727fc93bf700dae86107ef
Shivit Kapoor <shivit2004@gmail.com>
2025-02-27 16:35:40 +0000

    [sk3122] - Leaving exercise midway resets cached time

--- llm_based_sat_app/lib/widgets/exercise_widgets/exercise_bottom_message.dart
+++ llm_based_sat_app/lib/widgets/exercise_widgets/exercise_bottom_message.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:llm_based_sat_app/theme/app_colours.dart';
+import 'package:llm_based_sat_app/utils/exercise_timer_manager.dart';
 
 import '../../main.dart';
 
-/* ExerciseBottomMessage is a widget that displays a cautionary message to the user and when clicked directs the user to the Courses page.
+/* ExerciseBottomMessage is a widget that displays a cautionary message to the user and when clicked directs the user to the Courses page and resets cached time.
 
 ## Usage
 ExerciseBottomMessage(messageText: 'Leave and lose your progress.');
@@ -15,15 +16,17 @@ ExerciseBottomMessage(messageText: 'Leave and lose your progress.');
 class ExerciseBottomMessage extends StatelessWidget {
   final String messageText; // Text to display
 
-  const ExerciseBottomMessage(
-      {super.key,
-      required this.messageText});
+  const ExerciseBottomMessage({super.key, required this.messageText});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        // Reset the navigation stack to Courses 
+        // Reset the timer
+        ExerciseTimerManager().stopTimer();
+        ExerciseTimerManager().resetTimer();
+        print("Time elapsed: ${ExerciseTimerManager().elapsedTimeMillis}");
+        // Reset the navigation stack to Courses
         Navigator.of(context).pushAndRemoveUntil(
           MaterialPageRoute(
             builder: (context) => MainScreen(initialIndex: 4),
