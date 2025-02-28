182626188a94503791ef322de0b354795ff16ade
nb1622 <nb1622@ic.ac.uk>
2025-02-28 19:25:26 +0000

    [nb1622] (feat) - Added Payment logging and also changed API keys and how they work

--- llm_based_sat_app/lib/main.dart
+++ llm_based_sat_app/lib/main.dart
@@ -20,6 +20,7 @@ import '../widgets/bottom_nav_bar.dart';
 import 'firebase/firebase_options.dart';
 import 'package:provider/provider.dart';
 import 'utils/profile_notifier.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 // Background message handler
 Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
@@ -35,6 +36,9 @@ void main() async {
   
   // Register background handler before initializing Firebase
   FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  
+  // Load environment variables
+  await dotenv.load(fileName: ".env");
 
   await _setup();  // Ensures Stripe and other initializations
 
@@ -49,6 +53,7 @@ void main() async {
     sound: true,
   );
 
+
   runApp(
     MultiProvider(
       providers: [
@@ -62,7 +67,7 @@ void main() async {
 }
 
 Future<void> _setup() async {
-  Stripe.publishableKey = stripePublishableKey;
+  Stripe.publishableKey = dotenv.env['STRIPE_PUBLISHABLE_KEY'] ?? '';
 }
 
 class MyApp extends StatefulWidget {
