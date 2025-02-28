5ef91f7ab3c484c2242781824c1b37e69813398b
Harini Sritharar <harinisri2004@gmail.com>
2025-02-28 15:34:35 +0000

    [hs1922,sk2922] fix: amend logic for calculating scores

--- llm_based_sat_app/lib/screens/score/questionnaire_page.dart
+++ llm_based_sat_app/lib/screens/score/questionnaire_page.dart
@@ -39,7 +39,6 @@ class _QuestionnairePageState extends State<QuestionnairePage> {
       setState(() {
         responses.clear();
         description = questionnaireData['description'] as String;
-        ;
         questions = questionnaireData['questions'] as List<String>;
       });
     }
