3ced81a7ca369c74140f2cecbe9b6ee308f4e826
Shivit Kapoor <shivit2004@gmail.com>
2025-03-04 12:51:19 +0000

    [sk3122] - Comments for data folder

--- llm_based_sat_app/lib/data/cache_manager.dart
+++ llm_based_sat_app/lib/data/cache_manager.dart
@@ -1,4 +1,9 @@
+/* An in-memory cache manager for storing and retrieving key-value pairs. This class provides basic caching functionality without any expiration policy. It can be used to store frequently accessed data to improve performance. */
 class CacheManager {
+  /* Stores a value in the cache with the specified key.
+  If a value already exists for the given key, it will be overwritten.
+  - [key]: The unique identifier for the cached item.
+  - [value]: The data to be stored in the cache. */
   static final Map<String, dynamic> _cache = {};
 
   static void setValue(String key, dynamic value) {
